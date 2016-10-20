7fb02f945b3b4f850bed006ce6bb6ea9214a56e1
Aaron Rice <adrice727@gmail.com>
2016-10-20 15:54:53 -0400

    Add ChatMemoryStorage.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -102,5 +102,39 @@ trait ChatManagement { this: Actor =>
   }
 }
 
+/**
+ * Abstraction of chat storage holding the chat log.
+ */
+trait ChatStorage extends Actor
+
+/**
+ * Memory-backed chat storage implementation.
+ */
+class ChatMemoryStorage extends ChatStorage {
+  self.lifecycle = Permanent
+
+  private var chatLog = TransactionalVector[Array[Byte]]()
+  EventHandler.info(this, "Memory-based chat storage is starting up . . .")
+
+  def receive = {
+    case msg @ ChatMessage(from, message) =>
+      EventHandler.debug(this, "New chat message [%s]".format(message))
+      atomic { chatLog + message.getBytes("UTF-8") }
+
+    case GetChatLog(_) =>
+      val messageList = atomic { chatLog.map(bytes => new String(bytes, "UTF-8")).toList }
+      self.reply(ChatLog(messageList))
+  }
+
+  override def postRestart(reason: Throwable) = chatLog = TransactionalVector()
+}
+
+/**
+  * Creates and links a MemoryChatStorage.
+  */
+trait MemoryChatStorageFactory { this: Actor =>
+  val storage = this.self.spawnLink[MemoryChatStorage]
+}
+
 
 
